a654493cbcc9e8ad815827c86296867b0ba43555
nick <nick@kiwi-hacker.net>
2020-12-03 21:06:46 +0000

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# duckdns
+
+https://www.duckdns.org/
+
+A little container to update duckdns
+
+## CREDS
+
+DOMAINS="domain1 domain2"
+TOKEN=ffffffff-aaaa-aaaa-99999-12345678
---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+. ./CREDS
+
+SLEEP=300
+
+while true;
+do
+
+    for DOMAIN in $DOMAINS ; do
+        URL="https://www.duckdns.org/update?domains=${DOMAIN}&token=${TOKEN}&ip=&verbose=true" 
+        echo $URL
+        curl -k $URL
+        echo ""
+    done
+    sleep $SLEEP
+done 
+
